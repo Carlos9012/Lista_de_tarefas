660e7c18517c6dcb9f7ba5a409eb1b052aee78db
Carlos9012 <francisco.carlos1564@gmail.com>
2023-06-04 18:12:20 -0300

    first commit

---
+++ Data/SistemaTarefasDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ListaDeTarefas.Models;
+using ListaDeTarefas.Data.Map;
+
+namespace ListaDeTarefas.Data
+{
+    public class SistemaTarefasDBContext : DbContext
+    {
+        public SistemaTarefasDBContext(DbContextOptions<SistemaTarefasDBContext> options)
+            : base(options) 
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefa { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new TarefaMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Enums/StatusTarefa.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace ListaDeTarefas.Enums
+{
+    public enum StatusTarefa
+    {
+        [Description("A fazer")]
+        Afazer = 1,
+        [Description("Em andamento")]
+        EmAndamento = 2,
+        [Description("Concluido")]
+        Cocluido = 3
+
+    }
+}
